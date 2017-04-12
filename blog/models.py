3485c6fcb35f56d94434292b03bfae8dbaaa9649
lalitmee <lalitkumar.meena.lk@gmail.com>
2017-04-12 15:17:21 +0530

    Added user register and login

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import Permission, User
 from django.utils import timezone
 # Create your models here.
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
+    #user = models.ForeignKey(User, default=1)
+    author = models.ForeignKey('auth.User', default=1)
     title = models.CharField(max_length = 200)
     text = models.TextField()
     created_date = models.DateTimeField(default = timezone.now)
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from .forms import PostForm
+from .forms import PostForm, UserForm
+from django.contrib.auth import logout
 from django.shortcuts import redirect
 from django.utils import timezone
 from .models import Post
@@ -17,53 +19,106 @@ def post_detail(request, pk):
 
 @login_required
 def post_new(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect('post_detail', pk=post.pk)
+    if not request.user.is_authenticated():
+        return render(request, 'blog/login.html')
 
     else:
-        form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+        if request.method == "POST":
+            form = PostForm(request.POST)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.save()
+                return redirect('post_detail', pk=post.pk)
+
+        else:
+            form = PostForm()
+        return render(request, 'blog/post_edit.html', {'form': form})
 
 @login_required
 def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect('post_detail', pk=post.pk)
+    if not request.user.is_authenticated():
+        return render(request, 'blog/login.html')
     else:
-        form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+        post = get_object_or_404(Post, pk=pk)
+        if request.method == "POST":
+            form = PostForm(request.POST, instance=post)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.save()
+                return redirect('post_detail', pk=post.pk)
+        else:
+            form = PostForm(instance=post)
+        return render(request, 'blog/post_edit.html', {'form': form})
 
 @login_required
 def post_draft_list(request):
-    post = Post.objects.filter(published_date__isnull=True).order_by('created_date')
-    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+    if not request.user.is_authenticated():
+        return render(request, 'blog/login.html')
+    else:
+        posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+        return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
 @login_required
 def post_publish(request, pk):
-    post = get_object_or_404(Post, pk = pk)
-    post.publish()
-    return redirect('post_detail', pk = pk)
+    if not request.user.is_authenticated():
+        return render(request, 'blog/login.html')
+    else:
+        post = get_object_or_404(Post, pk = pk)
+        post.publish()
+        return redirect('post_detail', pk = pk)
 
 @login_required
 def post_remove(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    post.delete()
-    return redirect('post_list')
+    if not request.user.is_authenticated():
+        return render(request, 'blog/login.html')
+    else:
+        post = get_object_or_404(Post, pk=pk)
+        post.delete()
+        return redirect('post_list')
 
-from django.contrib.auth import logout
-
-@login_required
+#@login_required
 def logout(request):
     logout(request)
-    return redirect(request, 'blog/post_list.html')
-    # Redirect to a success page.
+    form = UserForm(request.POST or None)
+    context = {
+        "form": form,
+    }
+    #print("Hello")
+    return render(request, 'blog/login.html', context)
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                posts = Post.objects.filter(user=request.user)
+                return render(request, 'blog/post_list.html', {'posts': posts})
+            else:
+                return render(request, 'blog/login.html', {'error_message': 'Your account has been disabled'})
+        else:
+            return render(request, 'blog/login.html', {'error_message': 'Invalid login'})
+    return render(request, 'blog/login.html')
+
+def register(request):
+    form = UserForm(request.POST or None)
+    if form.is_valid():
+        user = form.save(commit=False)
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        user.set_password(password)
+        user.save()
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                posts = Post.objects.filter(user=request.user)
+                return render(request, 'blog/post_list.html', {'posts': posts})
+    context = {
+        "form": form,
+    }
+    return render(request, 'blog/register.html', context)
